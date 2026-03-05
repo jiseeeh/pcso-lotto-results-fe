b97b6c0935cf3d782ceba882670b70be0803883d
stackovermaw <johncarlo.camara1@gmail.com>
2026-03-05 23:23:19 +0800

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,6 @@
+import { Link } from "@tanstack/react-router";
+import ThemeToggle from "./ThemeToggle";
+
+export default function Header() {
+  return <header className="sticky top-0 z-50"></header>;
+}
